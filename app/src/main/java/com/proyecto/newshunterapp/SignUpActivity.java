089a4081c92b7f77cdff2dbcd0ce8a1920be2841
Nyubei <javier.rojas.j@outlook.com>
2022-11-16 19:39:54 -0300

    a

--- app/src/main/java/com/proyecto/newshunterapp/SignUpActivity.java
+++ app/src/main/java/com/proyecto/newshunterapp/SignUpActivity.java
@@ -2,12 +2,26 @@ package com.proyecto.newshunterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextView vusername, vpass, vmail, vocupacion;
+    private MaterialButton btnRegistro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +40,36 @@ public class SignUpActivity extends AppCompatActivity {
                 findViewById(R.id.dropdown);
         editTextFilledExposedDropdown.setAdapter(adapter);
 
+        vusername = findViewById(R.id.vUserName);
+        vpass = findViewById(R.id.vUserPass);
+        vmail = findViewById(R.id.vUserMail);
+        vocupacion = findViewById(R.id.dropdown);
+        btnRegistro = findViewById(R.id.btnRegistro);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    String username = vusername.getText().toString();
+                    String pass = vpass.getText().toString();
+                    String mail = vmail.getText().toString();
+                    String ocupacion = vocupacion.getText().toString();
+
+                    SQLiteDatabase db = openOrCreateDatabase("BD_USER", Context.MODE_PRIVATE,null);
+
+                    String sql = "insert into user(username,pass,mail,ocupacion)values(?,?,?,?)";
+                    SQLiteStatement statement = db.compileStatement(sql);
+                    statement.bindString(1,username);
+                    statement.bindString(2,pass);
+                    statement.bindString(3,mail);
+                    statement.bindString(4,ocupacion);
+                    statement.execute();
+                    Toast.makeText(SignUpActivity.this,"Usuario Registrado", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception ex){
+                    Toast.makeText(SignUpActivity.this,"Usuario no pudo ser Registrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/proyecto/newshunterapp/WriteActivity.java
+++ app/src/main/java/com/proyecto/newshunterapp/WriteActivity.java
@@ -2,13 +2,60 @@ package com.proyecto.newshunterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class WriteActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        MaterialButton btnContinuar = (MaterialButton) findViewById(R.id.btnContinuar);
+
+        EditText txtTitle = (EditText) findViewById(R.id.vNewsTittle);
+        EditText txtBody = (EditText) findViewById(R.id.vBodyNews);
+
+        txtBody.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(txtBody.hasFocus()) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    switch (event.getAction() & MotionEvent.ACTION_MASK){
+                        case MotionEvent.ACTION_SCROLL:
+                            v.getParent().requestDisallowInterceptTouchEvent(false);
+                            return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strTitle = txtTitle.getText().toString();
+                String strBody = txtBody.getText().toString();
+
+                if((TextUtils.isEmpty(strTitle) || TextUtils.isEmpty(strBody))){
+                    Toast.makeText(WriteActivity.this,"Debe completar los campos",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    startActivity(new Intent(WriteActivity.this, WriteActivity2.class));
+                }
+            }
+        });
     }
 }
